e9a111041e289a29c42b0d1a7774da514f8b644c
dylandmp20 <dylandmp20@gmail.com>
2023-06-13 21:01:13 -0500

    Calificacion_Estudiante

---
+++ src/main/java/pe/edu/upc/connection2connection/entities/Calificacion_Estudiante.java
@@ -0,0 +1,51 @@
+package pe.edu.upc.connection2connection.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Calificacion_Estudiante")
+public class Calificacion_Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "Estudiante_id")
+    private Estudiante estudiante;
+    @ManyToOne
+    @JoinColumn(name = "Calificacion_id")
+    private Calificacion calificacion;
+
+    public Calificacion_Estudiante(){
+
+    }
+
+    public Calificacion_Estudiante(int id, Estudiante estudiante, Calificacion calificacion) {
+        this.id = id;
+        this.estudiante = estudiante;
+        this.calificacion = calificacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Calificacion getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Calificacion calificacion) {
+        this.calificacion = calificacion;
+    }
+}
