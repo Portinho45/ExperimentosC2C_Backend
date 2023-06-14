dd4c149590f8fc071884b5704ae1687b64e33c07
katlp48 <katylezameta@gmail.com>
2023-06-13 23:09:00 -0500

    Estudiante-Usuario-Backend

--- src/main/java/pe/edu/upc/connection2connection/dtos/EstudianteDTO.java
+++ src/main/java/pe/edu/upc/connection2connection/dtos/EstudianteDTO.java
@@ -5,13 +5,13 @@ import pe.edu.upc.connection2connection.entities.Usuario;
 
 public class EstudianteDTO {
     private int idEstudiante;
-    private int Semestre_Estudiante;
-    private int Edad_Estudiante;
-    private String Genero_Estudiante;
-    private boolean Practicante_Estudiante;
-    private String Descripcion_Estudiante;
-    private Institucion Institucion_Estudiante;
-    private Usuario Usuario_Estudiante;
+    private int semestre_Estudiante;
+    private int edad_Estudiante;
+    private String genero_Estudiante;
+    private boolean practicante_Estudiante;
+    private String descripcion_Estudiante;
+    private Institucion institucion_Estudiante;
+    private Usuario usuario_Estudiante;
 
     public int getIdEstudiante() {
         return idEstudiante;
@@ -22,58 +22,58 @@ public class EstudianteDTO {
     }
 
     public int getSemestre_Estudiante() {
-        return Semestre_Estudiante;
+        return semestre_Estudiante;
     }
 
     public void setSemestre_Estudiante(int semestre_Estudiante) {
-        Semestre_Estudiante = semestre_Estudiante;
+        this.semestre_Estudiante = semestre_Estudiante;
     }
 
     public int getEdad_Estudiante() {
-        return Edad_Estudiante;
+        return edad_Estudiante;
     }
 
     public void setEdad_Estudiante(int edad_Estudiante) {
-        Edad_Estudiante = edad_Estudiante;
+        this.edad_Estudiante = edad_Estudiante;
     }
 
     public String getGenero_Estudiante() {
-        return Genero_Estudiante;
+        return genero_Estudiante;
     }
 
     public void setGenero_Estudiante(String genero_Estudiante) {
-        Genero_Estudiante = genero_Estudiante;
+        this.genero_Estudiante = genero_Estudiante;
     }
 
     public boolean isPracticante_Estudiante() {
-        return Practicante_Estudiante;
+        return practicante_Estudiante;
     }
 
     public void setPracticante_Estudiante(boolean practicante_Estudiante) {
-        Practicante_Estudiante = practicante_Estudiante;
+        this.practicante_Estudiante = practicante_Estudiante;
     }
 
     public String getDescripcion_Estudiante() {
-        return Descripcion_Estudiante;
+        return descripcion_Estudiante;
     }
 
     public void setDescripcion_Estudiante(String descripcion_Estudiante) {
-        Descripcion_Estudiante = descripcion_Estudiante;
+        this.descripcion_Estudiante = descripcion_Estudiante;
     }
 
     public Institucion getInstitucion_Estudiante() {
-        return Institucion_Estudiante;
+        return institucion_Estudiante;
     }
 
     public void setInstitucion_Estudiante(Institucion institucion_Estudiante) {
-        Institucion_Estudiante = institucion_Estudiante;
+        this.institucion_Estudiante = institucion_Estudiante;
     }
 
     public Usuario getUsuario_Estudiante() {
-        return Usuario_Estudiante;
+        return usuario_Estudiante;
     }
 
     public void setUsuario_Estudiante(Usuario usuario_Estudiante) {
-        Usuario_Estudiante = usuario_Estudiante;
+        this.usuario_Estudiante = usuario_Estudiante;
     }
 }
