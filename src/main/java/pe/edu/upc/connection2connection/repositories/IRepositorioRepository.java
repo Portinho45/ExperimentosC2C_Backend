4665c2e048ba04ce52eaf2f01005c415806730dc
Portinho45 <xtrevator@gmail.com>
2023-06-23 17:25:20 -0500

    Repositorio y reporte02 finalizado correccion2

--- src/main/java/pe/edu/upc/connection2connection/repositories/IRepositorioRepository.java
+++ src/main/java/pe/edu/upc/connection2connection/repositories/IRepositorioRepository.java
@@ -9,11 +9,11 @@ import java.util.List;
 
 @Repository
 public interface IRepositorioRepository extends JpaRepository<Repositorio, Integer> {
-    @Query(value = "SELECT u.Nombre_Usuario AS NombreEstudiante, e.Edad_estudiante AS Edad, e.Practicante_estudiante AS PracticanteEstudiante, COUNT(r.id) AS CantidadRepositorios\n" +
+    @Query(value = "SELECT u.Nombre_Usuario AS NombreEstudiante,u.correo_usuario AS CorreoEstudiante, e.Edad_estudiante AS Edad, e.Practicante_estudiante AS PracticanteEstudiante, COUNT(r.id) AS CantidadRepositorios\n" +
             "FROM usuarios u\n" +
             "JOIN estudiantes e ON u.id_usuario = e.usuario_id\n" +
             "LEFT JOIN repositorios r ON e.id_estudiante = r.id_estudiante\n" +
-            "GROUP BY u.Nombre_Usuario, e.Edad_estudiante, e.Practicante_estudiante;", nativeQuery = true)
+            "GROUP BY u.Nombre_Usuario, u.correo_usuario, e.Edad_estudiante, e.Practicante_estudiante;", nativeQuery = true)
     List<String[]> obtenerNumRepositorios();
 
 }
